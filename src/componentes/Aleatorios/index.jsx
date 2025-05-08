65abb5aa7b6d4bf646cd5b7aaf4013bde375057b
valnqzz <paulanieto270@gmail.com>
2025-05-08 02:48:32 +0000

    7marzo2

--- src/componentes/Aleatorios/index.jsx
+++ src/componentes/Aleatorios/index.jsx
@@ -1,15 +1,57 @@
-import { useState } from 'react'
-import './style.css'
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from '../../contexto/contexto';
 
 function Aleatorios() {
+  const { data, listaCapturados ,setListaCapturados, setTipoSeleccionado } = useContext(AppContext);
+  const [aleatorio, setAleatorio] = useState([])
+  
+  setTipoSeleccionado("All")
+  useEffect(() => {
+    if (data.length > 0) {
+      generar();
+    }
+  }, [data]);
+/*
+  useEffect(() => {
+    console.log("Actualizado:", listaCapturados);
+  }, [listaCapturados]);
+*/
+  const navigate = useNavigate();
 
+  const generar = () => {
+    let nuevosAleatorios = [];
+
+    while (nuevosAleatorios.length < 4) {
+      const index = Math.floor(Math.random() * data.length);
+      nuevosAleatorios.push(data[index]);
+    }
+    setAleatorio(nuevosAleatorios);
+
+    const nuevosIds = nuevosAleatorios.map(nuevoAleatorio => nuevoAleatorio.url.split("/")[6]).filter(id => !listaCapturados.includes(id));
+    setListaCapturados(prev => [...prev, ...nuevosIds]);
+  };
 
   return (
-    <>
-      
-    <h1>Aleatorios</h1>
-    </>
-  )
+    <section className="c-aleatorio c-lista">
+      {aleatorio.map((pokemon ,index) => (
+        <div className="c-lista-pokemon c-un_aleatorio" 
+        key={index}
+        onClick={() => navigate(`/detalle/${pokemon.name}`)}
+        >
+          <p>{pokemon.url.split("/")[6]}</p>
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.url.split("/")[6]}.png`}
+            alt={`Pokemon ${pokemon.name}`}
+            width="200"
+            height="200"
+          />
+          <p>{pokemon.name}</p>
+        </div>
+      ))}
+      <button onClick={generar}>Generar</button>
+    </section>
+  );
 }
 
-export default Aleatorios
+export default Aleatorios;
--- src/componentes/Original/index.jsx
+++ src/componentes/Original/index.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import './style.css'
+import { useState, useEffect, useContext } from 'react'
+import { useNavigate } from "react-router-dom";
+import { AppContext } from '../../contexto/contexto';
+import "./style.css"
 
 function Original() {
-
-
-  return (
-    <>
-      
-    <h1>Original</h1>
-    </>
-  )
-}
-
-export default Original
+    const { listaCapturados, totalPokes } = useContext(AppContext);
+    const espacio = Array.from({ length: totalPokes }, (_, i) => i + 1);
+    const navigate = useNavigate();
+    return (
+      <>
+      {listaCapturados.length} / {totalPokes}
+      <section className="c-aleatorio c-lista">
+      {espacio.map((id) => (
+        <div
+          key={id}
+          className={listaCapturados.includes(id.toString()) ? "c-unpoke c-mios-pokemon" : "c-unpoke"}
+          onClick={() => listaCapturados.includes(id.toString()) ? navigate(`/detalle/${id}`) : null}
+        >
+          {listaCapturados.includes(id.toString()) ? (
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+              width="auto"
+              height="45"
+              loading="lazy"
+              alt={id}
+            />
+          ) : null}
+          
+          <p>{id}</p>
+        </div>
+      ))}
+      </section>
+      </>
+    )
+  }
+  
+  export default Original
